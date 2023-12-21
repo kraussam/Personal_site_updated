fd1b95844ff49f5babca146886c1c6c400e9d433
kraussam <kraussam.04@gmail.com>
2023-12-21 13:17:02 -0500

    Could not get dynamic loading to work on chrome. Disabled it in the end. Works on firefox & safari.

--- js/dynamic_loading.js
+++ js/dynamic_loading.js
@@ -1,4 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
+    // Check if the browser is Chrome
+    const isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+
     // Function to handle the intersection observer callback
     function handleIntersection(entries, observer) {
         entries.forEach(entry => {
@@ -11,22 +14,24 @@ document.addEventListener('DOMContentLoaded', function () {
                 // Remove the transition property after fade-in
                 setTimeout(() => {
                     img.style.transition = '';
-                }, 500); // Assuming the transition duration is 0.5s
+                }, 2000); // Assuming the transition duration is 0.5s
             }
         });
     }
 
-    // Create an Intersection Observer
-    const observer = new IntersectionObserver(handleIntersection, { threshold: 0.5 });
+    // Create an Intersection Observer if not using Chrome
+    if (!isChrome) {
+        const observer = new IntersectionObserver(handleIntersection, { threshold: 0.5 });
 
-    // Apply fade-in effect for all images after they have loaded
-    document.querySelectorAll('img').forEach(img => {
-        img.style.opacity = 0; // Set initial opacity to 0
-        img.addEventListener('load', function () {
-            setTimeout(() => {
-                img.style.transition = 'opacity 0.5s ease-in-out'; // Isolate opacity transition
-                observer.observe(img); // Observe the image for dynamic loading effect
-            }, 1000); // Adjust the delay time as needed
+        // Apply fade-in effect for all images after they have loaded
+        document.querySelectorAll('img').forEach(img => {
+            img.style.opacity = 0; // Set initial opacity to 0
+            img.addEventListener('load', function () {
+                setTimeout(() => {
+                    img.style.transition = 'opacity 0.5s ease-in-out'; // Isolate opacity transition
+                    observer.observe(img); // Observe the image for dynamic loading effect
+                }, 1000); // Adjust the delay time as needed
+            });
         });
-    });
+    }
 });
